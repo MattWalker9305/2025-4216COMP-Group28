49fe9b8e548e75a4d51502b6b329e29847fff6e6
Matthew <matthewwalker194@gmail.com>
2025-03-25 16:51:40 +0000

    created function to call in menu

--- matt_walker_cw_q1.py
+++ matt_walker_cw_q1.py
@@ -5,56 +5,57 @@ import matplotlib.dates as mdates
 import numpy as np
 import datetime as dt
 
-team_not_found = True
+def team_season_points():
+    team_not_found = True
 
-selected_team = input("Enter team: ")
-selected_year = input("Enter year: ")
+    selected_team = input("Enter team: ")
+    selected_year = input("Enter year: ")
 
-with open('datasets/teams.csv', 'r') as f:
-    csv_reader = csv.reader(f)
-    next(csv_reader)
-    for row in csv_reader:
-        team = row[1]
-        team_id = row[0]
-        if team == selected_team:
+    with open('datasets/teams.csv', 'r') as f:
+        csv_reader = csv.reader(f)
+        next(csv_reader)
+        for row in csv_reader:
+            team = row[1]
             team_id = row[0]
-            team_not_found = False
-            break
+            if team == selected_team:
+                team_id = row[0]
+                team_not_found = False
+                break
 
-if team_not_found:
-    print("no team found")
-else:
-    match_results = []
- 
-    with open('datasets/teamstats.csv', 'r') as f:
-        teamstats = csv.reader(f)
-        next(teamstats)
+    if team_not_found:
+        print("no team found")
+    else:
+        match_results = []
+    
+        with open('datasets/teamstats.csv', 'r') as f:
+            teamstats = csv.reader(f)
+            next(teamstats)
+            
+            for row in teamstats:
+                match_team_id = row[1]
+                result = row[15]
+                season = row[2]
+                date = row[3]
+                if match_team_id == team_id and selected_year == season:
+                    match_results.append((date, result))
         
-        for row in teamstats:
-            match_team_id = row[1]
-            result = row[15]
-            season = row[2]
-            date = row[3]
-            if match_team_id == team_id and selected_year == season:
-                match_results.append((date, result))
+        match_results.sort(key=lambda x: x[0])
+
+        data = pd.DataFrame(match_results, columns=['date', 'result'])
+        data['date'] = pd.to_datetime(data['date'])
+        
+        data['points'] = data['result'].map({'W': 3, 'D': 1, 'L': 0})
     
-    match_results.sort(key=lambda x: x[0])
+        data['cumulative_points'] = data['points'].cumsum()
 
-    data = pd.DataFrame(match_results, columns=['date', 'result'])
-    data['date'] = pd.to_datetime(data['date'])
-    
-    data['points'] = data['result'].map({'W': 3, 'D': 1, 'L': 0})
-   
-    data['cumulative_points'] = data['points'].cumsum()
+        fig, ax = plt.subplots()
+        ax.xaxis.set_major_formatter(mdates.DateFormatter("%d.%m.%Y"))
+        ax.set_xticks(data['date'])
+        ax.plot(data['date'], data['cumulative_points'], 'go:')
+        ax.tick_params(axis='x', labelrotation=90)
 
-    fig, ax = plt.subplots()
-    ax.xaxis.set_major_formatter(mdates.DateFormatter("%d.%m.%Y"))
-    ax.set_xticks(data['date'])
-    ax.plot(data['date'], data['cumulative_points'], 'go:')
-    ax.tick_params(axis='x', labelrotation=90)
+        ax.set_xlabel('Date')
+        ax.set_ylabel('Points')
 
-    ax.set_xlabel('Date')
-    ax.set_ylabel('Points')
-
-    print(data)
-    plt.show()
+        print(data)
+        plt.show()
