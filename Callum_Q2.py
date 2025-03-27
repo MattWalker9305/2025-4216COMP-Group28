44a9319f87f68d30ec3527d848cf41fc34a6ca7c
Callum Colquitt <callumcolquitt@gmail.com>
2025-03-27 12:08:16 +0000

    used classes for dataset file to optimise code

--- Callum_Q2.py
+++ Callum_Q2.py
@@ -2,6 +2,7 @@ import csv
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import classes_for_dataset as cfd
 
 team_not_found = True
 
@@ -9,40 +10,26 @@ requested_team = input("Enter team:")
 requested_year = input("Enter year:")
 
 
-with open("2025-4216COMP-Group28/datasets/teams.csv", "r") as f:
-    csv_reader = csv.reader(f)
-    next(csv_reader)
-    for row in csv_reader:
-        team = row[1]
-        team_id = row[0]
-        if team == requested_team:
-            team_id = row[0]
-            team_not_found = False
-            break
-    if team_not_found:
+teams = cfd.read_file_to_array('2025-4216COMP-Group28/datasets/teams.csv', cfd.Team)
+for row in teams:
+    if row.name == requested_team:
+         team_id = row.teamID
+         team_not_found = False
+         break
+if team_not_found:
         print("no team found")
-    else:
+else:
         dates = []
         shot_conversion_rate = []
 
       
-        with open("2025-4216COMP-Group28/datasets/teamstats.csv", "r") as f:
-            team_stats = csv.reader(f)
-            next(team_stats)
-
-           
-            for row in team_stats:
-                current_team_id = row[1]
-                season = row[2]
-                shots = int(row[7])
-                goals = int(row[5])
-                date = row[3]
-
-                
-                if current_team_id == team_id and requested_year == season:
-                    if shots > 0:
-                        dates.append(date)
-                        shot_conversion_rate.append((goals / shots) * 100)
+        team_stats = cfd.read_file_to_array('2025-4216COMP-Group28/datasets/teamstats.csv', cfd.TeamStatistic)
+        for row in team_stats:
+ 
+                if row.teamID == team_id and requested_year == row.season:
+                    if int(row.shots) > 0:
+                        dates.append(row.date)
+                        shot_conversion_rate.append(float(row.goals) / float(row.shots) * 100)
 
         
         if dates:
