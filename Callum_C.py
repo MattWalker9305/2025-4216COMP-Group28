b597300c85baf2c7af9bc6f8b49af6439faac4e4
Callum Colquitt <callumcolquitt@gmail.com>
2025-03-27 12:44:28 +0000

    optimised question one by using classes for dataset

--- Callum_C.py
+++ Callum_C.py
@@ -1,46 +1,39 @@
 import csv
 import pandas as pd
 import matplotlib.pyplot as plt
+import classes_for_dataset as cfd
 def callum_C1():
     team_not_found = True
 
     requested_team = input("Enter team:")
     requested_year = input("Enter year:")
 
-    with open("2025-4216COMP-Group28/datasets/teams.csv", "r") as f:
-        csv_reader = csv.reader(f)
-        next(csv_reader)
-        for row in csv_reader:
-            team = row[1]
-            team_id = row[0]
-            if team == requested_team:
-                team_id = row[0]
-                team_not_found = False
-                break
+    teams = cfd.read_file_to_array('2025-4216COMP-Group28/datasets/teams.csv', cfd.Team)
+    for row in teams:
+        if row.name == requested_team:
+            team_id = row.teamID
+            team_not_found = False
+            break
 
-        if team_not_found:
+    if team_not_found:
             print("no team found")
-        else:
+    else:
             homeGoals = []
             awayGoals = []
 
-            with open("2025-4216COMP-Group28/datasets/games.csv", "r") as f:
-                games = csv.reader(f)
-                next(games)
+            games = cfd.read_file_to_array('2025-4216COMP-Group28\datasets\games.csv', cfd.Game)
+                
 
-                for row in games:
-                    home_team_id = row[4]
-                    away_team_id = row[5]
-                    home_goals = int(row[6])
-                    away_goals = int(row[7])
-                    season = row[2]
-                    if home_team_id == team_id and requested_year == season:
-                        homeGoals.append(home_goals)
-                    elif away_team_id == team_id and requested_year==season:
-                        awayGoals.append(away_goals)
+            for row in games:
+                   
+                    if row.homeTeamID == team_id and requested_year == row.season:
+                        homeGoals.append(row.homeGoals)
+                    elif row.awayTeamID == team_id and requested_year == row.season:
+                        awayGoals.append(row.awayGoals)
+            
 
-            total_home_goals = sum(homeGoals)
-            total_away_goals = sum(awayGoals)
+            total_home_goals = sum(list(map(int, homeGoals)))
+            total_away_goals = sum(list(map(int, awayGoals)))
 
         
             #bar chart
