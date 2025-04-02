3989aae034e19e69f165c0a9c94d808a9539eaa1
MattWalker9305 <matthewwalker194@gmail.com>
2025-04-03 00:08:06 +0100

    completed q2

--- matt_walker_cw.py
+++ matt_walker_cw.py
@@ -53,7 +53,6 @@ def team_season_points():
         
 def player_xG_VS_G():
     selected_season = int(input("Enter a season (2014-2020):"))
-    #start_time=time.time()
     games = cfd.read_file_to_array('datasets/games.csv', cfd.Game, filter_func=lambda row: int(row['season']) == selected_season)
     game_ids = []
     for game in games: game_ids.append(game.gameID)
@@ -64,12 +63,37 @@ def player_xG_VS_G():
     for appearance in appearances:
         player_groups[appearance.playerID].append(appearance)
 
+    player_data = []
+    player_dict = {player.playerID: player.name for player in players}
 
-    data = pd.DataFrame.from_dict(player_groups, orient='index')
+    for player_id, appearances in player_groups.items():
+        total_xG = sum([float(appearance.xGoals) for appearance in appearances])
+        total_goals = sum([int(appearance.goals) for appearance in appearances])
+
+        player_name = player_dict.get(player_id, "Unknown Player")
+
+        player_data.append({
+            'playerID': player_id,
+            'playerName': player_name,
+            'total_xG': total_xG,
+            'total_goals': total_goals
+        })
+    
+    data = pd.DataFrame(player_data)
     print(data)
-            
-    #end_time=time.time()
-    #print(f"done {end_time - start_time:.6f} seconds" )
 
-    # for performance in best_player_performance: print(performance.playerID)
-player_xG_VS_G()
+    top_players = data.sort_values(by='total_xG', ascending=False).head(10)
+    fig, ax = plt.subplots()
+    ax.scatter(data['total_xG'], data['total_goals'])
+    ax.scatter(top_players['total_xG'], top_players['total_goals'], color = 'red')
+
+    for i, row in top_players.iterrows():
+        ax.text(row['total_xG'], row['total_goals'], row['playerName'], fontsize=8)
+
+    ax.set_xlim([min(data['total_xG']) - 1, max(data['total_xG']) + 1])
+    ax.set_ylim([min(data['total_goals']) - 1, max(data['total_goals']) + 1])
+
+    ax.set_xlabel("Players total expected goals")
+    ax.set_ylabel("Players total goals")
+    ax.set_title(f"xG vs Goals for players in {selected_season}")
+    plt.show()
