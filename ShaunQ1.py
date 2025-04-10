09f75ce887c98692d588d19677caefe6c4f993b7
ShaunBlackburn123 <csmsblac@ljmu.ac.uk>
2025-04-10 22:12:52 +0100

    Added validation to q1

--- ShaunQ1.py
+++ ShaunQ1.py
@@ -2,18 +2,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import classes_for_dataset as cfd
+import time
 
 #creating funtion for the menu to import 
 def ShaunQ1Program():
 #getting input from user
     chosenPlayer = input("Players full name: ")
     chosenSeason = input("Season: ")
-    
+
+#validation 
+    playerNotFound = True
+    seasonNotFound = True
+
 #getting the player ID of the chosen player
     players = cfd.read_file_to_array('datasets/players.csv',cfd.Player)
     for player in players:
         if chosenPlayer == player.name:
             playerID = player.playerID
+            playerNotFound = False
             break
 
 #getting game ID's from users chosen season   
@@ -22,26 +28,35 @@ def ShaunQ1Program():
     for game in games:
         if chosenSeason == game.season:
             gameIDs.append(game.gameID)
+            seasonNotFound = False
 
-#storing players goals from each of their games into an array 
-    goals = []
-    appearances = cfd.read_file_to_array('datasets/appearances.csv', cfd.Appearance)
-    for appearance in appearances:
-        if playerID == appearance.playerID and appearance.gameID in gameIDs:
-            goals.append(int(appearance.goals))
+#Telling the user if there is an invalid input
+    if playerNotFound == True:
+        print("Player not found")
+        time.sleep(3)
+    elif seasonNotFound == True:
+        print("Season not found")
+        time.sleep(3)
+    else:
 
-#creating an array that adds the players goals along the season
-    cumulativeGoals = np.cumsum(goals)
+        #storing players goals from each of their games into an array 
+            goals = []
+            appearances = cfd.read_file_to_array('datasets/appearances.csv', cfd.Appearance)
+            for appearance in appearances:
+                if playerID == appearance.playerID and appearance.gameID in gameIDs:
+                    goals.append(int(appearance.goals))
 
-#variables to use as the axis for the line chart
-    x_values = list(range(1, len(goals) + 1))
-    y_values = cumulativeGoals
+        #creating an array that adds the players goals along the season
+            cumulativeGoals = np.cumsum(goals)
 
-#plotting the visualisation with title and labels
-    plt.plot(x_values, y_values)
-    plt.title(chosenPlayer + "'s goals over the " + chosenSeason + " season")
-    plt.xlabel("Number of games")
-    plt.ylabel("Number of goals")
-    plt.show()
+        #variables to use as the axis for the line chart
+            x_values = list(range(1, len(goals) + 1))
+            y_values = cumulativeGoals
 
-    
+        #plotting the visualisation with title and labels
+            plt.plot(x_values, y_values)
+            plt.title(chosenPlayer + "'s goals over the " + chosenSeason + " season")
+            plt.xlabel("Number of games")
+            plt.ylabel("Number of goals")
+
+            plt.show()
